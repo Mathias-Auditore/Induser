2a5d3b61ecfab0e2fc4c8b9e274db56ca43fea39
MrRebel <mathias.auditore@gmail.com>
2022-12-08 20:17:05 -0300

    beta-1.3

--- js/main.js
+++ js/main.js
@@ -13,6 +13,11 @@ function mostrar_buscador(){
     bars_search.style.top = "80px";
     cover_ctn_search.style.display = "block";
     inputSearch.focus();
+
+    if (inputSearch.value === ""){
+        box_search.style.display = "none"
+    }
+    
 }
 
 // FUNCION PARA OCULTAR AL BUSCADOR
@@ -21,9 +26,45 @@ function ocutar_buscador(){
     bars_search.style.top = "-10px";
     cover_ctn_search.style.display = "none";
     inputSearch.value = "";
+    box_search.style.display = "none"
 }
 
 // EJECUTANDO
 
 document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+document.getElementById("icon_search").addEventListener("click", mostrar_buscador);
 document.getElementById("cover-ctn-search").addEventListener("click", ocutar_buscador);
+
+
+// Filtrado de busqueda
+
+
+
+function buscador_interno (){
+    filtro = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    // Recorriendo elementos a filtrar mediante los li
+    for (i = 0; i < li.length; i++){
+
+        a = li[i].getElementsByTagName("a")[0];
+        let textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filtro) > -1){
+
+            li[i].style.display = "";
+            box_search.style.display = "block";
+
+            if (inputSearch.value === ""){
+                box_search.style.display = "none"
+            }
+
+        }  else{
+            li[i].style.display = "none";
+        }
+
+    }
+
+}
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
