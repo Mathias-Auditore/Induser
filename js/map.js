dc7851c66a8021e5bab945d8ba61fa01cca5994c
Francisco-Robles <franciscojrobles26@gmail.com>
2023-08-05 13:46:06 -0300

    Agrego mapa y correcciones generales

--- js/map.js
+++ js/map.js
@@ -1,7 +1,6 @@
-
 let mapaInicial;
 
-const map = L.map('map').setView([-35.6118, -65.4173], 4);
+const map = L.map('map').setView([-38.6118, -65.4173], 4);
 
 function initMap() {
 
@@ -16,25 +15,38 @@ function initMap() {
 
     const puntosMarcados = [
         { lat: -34.7502149076031, lng: -58.42033731187113, nombre: 'Buenos Aires - Laboratorio central' },
-        { lat: -24.796249515643776, lng: -65.40694227696831, nombre: 'Salta' },
-        { lat: -32.912215090852726, lng: -68.8408925754632, nombre: 'Mendoza' },
-        { lat: -31.546679711809126, lng: -68.5369207755346, nombre: 'San Juan' },
-        { lat: -38.926004702429275, lng: -68.10521625274592, nombre: 'Neuquén' },
-        { lat: -45.7948537251709, lng: -67.47328540351415, nombre: 'Chubut' },
-        { lat: -46.45161686122602, lng: -67.50912941696151, nombre: 'Santa Cruz' },
-        { lat: -28.464300834039033, lng: -65.77699551801452, nombre: 'Catamarca' },
+        { lat: -24.796249515643776, lng: -65.40694227696831, nombre: 'Sede Salta' },
+        { lat: -32.912215090852726, lng: -68.8408925754632, nombre: 'Sede Mendoza' },
+        { lat: -31.546679711809126, lng: -68.5369207755346, nombre: 'Sede San Juan' },
+        { lat: -38.926004702429275, lng: -68.10521625274592, nombre: 'Sede Neuquén' },
+        { lat: -45.7948537251709, lng: -67.47328540351415, nombre: 'Sede Chubut' },
+        { lat: -46.45161686122602, lng: -67.50912941696151, nombre: 'Sede Santa Cruz' },
+        { lat: -28.464300834039033, lng: -65.77699551801452, nombre: 'Sede Catamarca' },
     ];
 
-    puntosMarcados.forEach(punto => {
-        const marker = L.marker([punto.lat, punto.lng]).addTo(map);
-        marker.bindPopup(punto.nombre);
-        marker.on('click', function () {
-            map.flyTo([punto.lat, punto.lng], 14, {
-                duration: 2,
-                easeLinearity: 0.25
+    fetch('./../sedes.json')
+    .then(response => response.json())
+    .then(data => {
+        const sedesData = data;
+        document.getElementById('nombreSede').textContent = sedesData.sedes[0].nombre;
+        document.getElementById('direccionSede').textContent = sedesData.sedes[0].direccion;
+        document.getElementById('contactoSede').textContent = sedesData.sedes[0].contacto;
+        puntosMarcados.forEach((punto, index) => {
+            const marker = L.marker([punto.lat, punto.lng]).addTo(map);
+            marker.bindPopup(punto.nombre);
+            marker.on('click', function () {
+                map.flyTo([punto.lat, punto.lng], 14, {
+                    duration: 2,
+                    easeLinearity: 0.25
+                });
+                document.getElementById('nombreSede').textContent = sedesData.sedes[index].nombre;
+                document.getElementById('direccionSede').textContent = sedesData.sedes[index].direccion;
+                document.getElementById('contactoSede').textContent = sedesData.sedes[index].contacto;
+                document.getElementById('sede').style.display = 'block';
             });
         });
-    });
+    })
+    .catch(error => console.error("Error al cargar el archivo JSON: ", error));
 
     const islasMalvinasIcon = L.divIcon({
         className: 'custom-label-icon',
